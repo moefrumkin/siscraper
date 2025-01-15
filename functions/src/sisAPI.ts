15c30e30c955a9e18074dc41af495bcc8a23f303
Moe Frumkin <moegf1@gmail.com>
2025-01-15 16:21:51 -0500

    everything works

--- functions/src/sisAPI.ts
+++ functions/src/sisAPI.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {defineString} from "firebase-functions/params";
-import { CourseDetailsQuery, CourseQuery, TermedCourseDetailsQuery } from "siscraper-shared";
+import {CourseDetailsQuery, CourseQuery, TermedCourseDetailsQuery} from "siscraper-shared";
 
 const APIKey = defineString("SIS_API_KEY");
 const APIBase = "https://sis.jhu.edu/api/classes";
@@ -9,20 +9,20 @@ const APIBase = "https://sis.jhu.edu/api/classes";
  * Creates a function that requests data from an enpoint.
  * Prepends the API base and appends the API Key
  * @param formatter
- * @returns 
+ * @returns
  */
 const request = <T>(formatter: ((arg: T) => string)) => (arg: T) =>
   axios.get(`${APIBase}/${formatter(arg)}?key=${APIKey.value()}`)
     .then((response) => response.data);
 
-const requestString = (route: string) => request<void>(() => route)
+const requestString = (route: string) => request<void>(() => route);
 
-export const requestSchools = requestString("codes/schools")
+export const requestSchools = requestString("codes/schools");
 
 export const requestDepartments =
-  request((school: string) => `codes/departments/${school}`)
+  request((school: string) => `codes/departments/${school}`);
 
-export const requestTerms = requestString(`codes/terms`)
+export const requestTerms = requestString("codes/terms");
 
 export const formatCourseQuery = (query: CourseQuery) => (
   [query.terms.map((term) => `Term=${term}`).join("&"),
@@ -35,19 +35,19 @@ export const formatCourseQuery = (query: CourseQuery) => (
     .join("&")
 );
 
-export const queryCourses = request((query: CourseQuery) =>
-  encodeURI(formatCourseQuery(query))
-)
+export const queryCourses = (query: CourseQuery) =>
+  axios.get(`${APIBase}?${formatCourseQuery(query)}&key=${APIKey.value()}`)
+    .then((response) => response.data);
 
 const formatCourseDetailsRequest = (query: TermedCourseDetailsQuery) =>
   `${query.courseNumber.replace(/\./g, "")}` +
   `${query.sectionNumber}` +
-  `${encodeURI(query.term)}`
+  `${encodeURI(query.term)}`;
 
-export const requestCourseDetails = request(formatCourseDetailsRequest)
+export const requestCourseDetails = request(formatCourseDetailsRequest);
 
 const formatCourseSectionsQuery = (query: CourseDetailsQuery) =>
   `${query.courseNumber.replace(/\./g, "")}` +
-  (query.term ? `${encodeURI(query.term)}` : "")
+  (query.term ? `${encodeURI(query.term)}` : "");
 
-export const requestCourseSections = request(formatCourseSectionsQuery)
+export const requestCourseSections = request(formatCourseSectionsQuery);
