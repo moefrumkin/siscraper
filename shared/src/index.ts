e105aa2cb41a222a56994daf554e89f66b1a14be
Moe Frumkin <moegf1@gmail.com>
2025-01-15 20:22:37 -0500

    document the shared library

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,22 +1,40 @@
+/**
+ * Represents a school
+ */
 export type School = {
     Name: string
     Departments: Array<Department>
 }
 
+/**
+ * Represents a department within the school
+ * This is in the format that the API will return
+ */
 export type Department = {
     DepartmentName: string
     SchoolName: string
 };
 
+/**
+ * A type guard for the `Department` type
+ * @param department The potential department
+ * @returns A type predicate
+ */
 export const isDepartment = (department: unknown): department is Department => (
   Object.prototype.hasOwnProperty.call(department, "DepartmentName") &&
     Object.prototype.hasOwnProperty.call(department, "SchoolName")
 )
 
+/**
+ * Represents a term
+ */
 export type Term = {
     Name: string
 }
 
+/**
+ * Represents a course
+ */
 export type Course = {
     AllDepartments: string,
     Areas: string,
@@ -56,6 +74,9 @@ export type Course = {
     Waitlisted: string,
 }
 
+/**
+ * Represents a detailed description of a specific section of a course
+ */
 export type SectionDetails = {
     CoRequisites: string[],
     Credits: string,
@@ -73,6 +94,9 @@ export type SectionDetails = {
     Equivalencies: unknown
 }
 
+/**
+ * Represents an individual class meeting
+ */
 export type Meeting = {
     Building: string,
     DOW: string,
@@ -82,24 +106,38 @@ export type Meeting = {
     Times: string
 }
 
+/**
+ * Represents a prerequisite for a course
+ */
 export type Prerequisite = {
     Description: string,
     Expression: string,
     IsNegative: string
 }
 
+/**
+ * Represents a search query for a course. All terms are unioned.
+ */
 export type SearchQuery = {
     terms: string[],
     schools: string[],
     departments: Department[]
 }
 
+/**
+ * Similar to SearchQuery
+ */
 export type CourseQuery = {
     terms: Array<string>,
     schools: Array<string>,
     departments: Array<Department>
 }
 
+/**
+ * A type guard for the `CourseQuery` type 
+ * @param context the potential query
+ * @returns a type predicate for `CourseQuery`
+ */
 export const isCourseQuery = (context: object): context is CourseQuery => {
   const isArrayOfString = (arr: unknown): arr is Array<string> => (
     Array.isArray(arr) && arr.every((el) => typeof el === "string")
@@ -114,15 +152,26 @@ export const isCourseQuery = (context: object): context is CourseQuery => {
         context.departments.every((dept: unknown) => isDepartment(dept));
 };
 
+/**
+ * A general query for a detailed description of a course
+ */
 export type CourseDetailsQuery = {
     courseNumber: string,
     sectionNumber: string,
     term?: string
 }
 
+/**
+ * A query for a detailed description of the course with a single term specified
+ */
 export type TermedCourseDetailsQuery = CourseDetailsQuery &
 { term: string }
 
+/**
+ * A type predicate for the `CourseDetailsQuery` type 
+ * @param query the potential course query type
+ * @returns A type predicate for `CourseDetailsQuery`
+ */
 export const isCourseDetailsQuery =
     (query: object): query is CourseDetailsQuery => (
       "courseNumber" in query &&
@@ -131,6 +180,11 @@ export const isCourseDetailsQuery =
         typeof query.sectionNumber === "string"
     );
 
+/**
+ * A type predicate for the `TermedCourseDetailsQuery` type 
+ * @param query the potential terms course query
+ * @returns A type predicate for `TermedCourseDetailsQuery` 
+ */
 export const isTermedCourseDetailsQuery =
     (query: object): query is TermedCourseDetailsQuery => (
       isCourseDetailsQuery(query) &&
@@ -139,32 +193,58 @@ export const isTermedCourseDetailsQuery =
     );
 
 
-// A utility type for the react-select Select component
+/**
+ * A type that represents a tagged base type
+ */
 export type Labeled<T> = {
     value: T,
     label: string
 }
 
+/**
+ * A function that returns a boolean, often indicating membership of some category
+ */
 export type Predicate<T> = (instance: T) => boolean
 
+/**
+ * A filter is a predicate, used to determine if a course has a certain property, and a name for the filter
+ */
 export type Filter<T> = {
     predicate: Predicate<T>,
     name: string
 }
 
+/**
+ * Constructs a predicate that determines if a specific field has a given value 
+ * @param value the value to determine if the field has 
+ * @param field the field in the course 
+ * @returns a `Predicate<T>` that determines whether the course has the given value
+ */
 const equals = <T>(value: T, field: keyof Course) => (course: Course) => course[field] === value
 
+/**
+ * Constructs a filter that determines if a given field has a specific string value 
+ * @param value the string value to compare the field against
+ * @param field the field to compare
+ * @returns a `Filter<string>` that determines whether the course has the given string value.
+ */
 const equalsString = (value: string, field: keyof Course) => ({
   predicate: equals(value, field),
   name: value
 })
 
+/**
+ * Metadata corresponding to a specific field in the `Course` object
+ */
 export type ColumnMeta = {
     name: keyof Course,
     readableName: string,
     filters?: Filter<Course>[]
 }
 
+/**
+ * The list of fields in the `Course` object with corresponding metadata
+ */
 export const CourseHeader: { [key in (keyof Course)]: ColumnMeta } = {
   TermStartDate: {
     name: "TermStartDate",
@@ -325,6 +405,9 @@ export const CourseHeader: { [key in (keyof Course)]: ColumnMeta } = {
   }
 }
 
+/**
+ * A list of default columns to display
+ */
 export const DefaultColumns: ColumnMeta[] = [
   CourseHeader.SchoolName,
   CourseHeader.Term,
